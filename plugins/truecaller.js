9ad05378a7d1b0c46b764106c6302a19c90fe730
Herbert40 <108845744+Herbert40@users.noreply.github.com>
2022-07-22 15:04:14 +0530

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -9,7 +9,7 @@ const s = require('../config');
 const wk = s.MODE == 'public' ? false : true
 Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',use: 'search',fromMe: wk}, async (msg, query) => {
 if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
-if (query[1].includes('/')) return await msg.sendReply('Wrong format! \n\n .true +91 6282344739')
+if (query[1].includes('/')) return await msg.sendReply('Wrong format! \n\n .true +91 8416093656')
 var go;
 if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
 else if (!query[1].includes('@')) go = query[1]
